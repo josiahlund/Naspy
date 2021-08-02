8de3ec59715d18c6a0bac386010b910c9bffa675
Josiah Lund <josiahlund@gmail.com>
2021-08-01 22:15:33 -0500

    Add read_card function

--- read_bdf.py
+++ read_bdf.py
@@ -1,10 +1,13 @@
 import warnings
 from BULKDATA.BulkDataEntry import BulkDataEntry
+from more_itertools import peekable
 
 
 def read_bdf(path):
     valid_cards = [scl.__name__.upper() for scl in BulkDataEntry.__subclasses__()]
     with open(path) as f:
+        # TODO figure out if there's a cleaner way to make f peekable.
+        f = peekable(f)
         in_bulk = False
         for line in f:
             if not in_bulk:
@@ -22,7 +25,40 @@ def read_bdf(path):
                 # Warn when unsupported card encountered
                 if card_image not in valid_cards:
                     warnings.warn(f'Unsupported card image found: {card_image}')
-
+                else:
+                    field_data = read_card(line, f)
         if not in_bulk:
             # BEGIN BULK is a required card for any valid Nastran job
             raise EOFError('No "BEGIN BULK" statement found.')
+
+
+def read_card(line: str, f) -> list:
+    card_data = []
+    while True:
+        # Determine card format
+        if "*" in line[:8]:
+            # Card is long format
+            fields_to_read = 4
+            field_width = 16
+        else:
+            # Card is short format
+            fields_to_read = 8
+            field_width = 8
+
+        card_data += [line[8 + i*field_width:8 + (i+1)*field_width] for i in range(fields_to_read)]
+
+        try:
+            line = f.peek()
+        except StopIteration:
+            # EOF
+            break
+        else:
+            # Check if line is empty or a comment, break
+            if not line.strip() or line.strip().startswith("$"):
+                break
+            # Check if line could be continuation of current card
+            elif not any(line[:8].startswith(c) for c in ["+", "*", " "]):
+                break
+            else:
+                line = next(f)
+    return card_data
