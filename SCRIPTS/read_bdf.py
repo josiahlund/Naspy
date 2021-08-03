e6ccbc6b10e3635b937e1a17218f123e5f082c1b
Josiah Lund <josiahlund@gmail.com>
2021-08-03 15:57:38 -0500

    Refactor bulk data read into function

--- SCRIPTS/read_bdf.py
+++ SCRIPTS/read_bdf.py
@@ -4,40 +4,16 @@ from more_itertools import peekable
 
 
 def read_bdf(path):
-    valid_cards = [scl.__name__.upper() for scl in BulkDataEntry.__subclasses__()]
-    cards_read = {}
     with open(path) as f:
         # TODO figure out if there's a cleaner way to make f peekable.
         f = peekable(f)
         in_bulk = False
         for line in f:
             if not in_bulk:
+                # Until case control cards are supported, skip all lines until BEGIN BULK statement has been read.
                 if line.strip().upper() == "BEGIN BULK":
                     in_bulk = True
-
-            # Until case control cards are supported, elif below (instead of if) also skips all lines until BULK DATA
-            # statement has been read.
-
-            # skip blank lines and comments
-            elif not line.strip() or line.strip().startswith("$"):
-                pass
-            else:
-                card_image = line[:8].strip().upper()
-                # Warn when unsupported card encountered
-                if card_image not in valid_cards:
-                    warnings.warn(f'Unsupported card image found: {card_image}')
-                else:
-                    field_data = read_card(line, f)
-                    # TODO see if theres a way to call a subclass directly (without having to import every subclass)
-                    # There's no need for this to be a list comprehension, I just want to run the subclass of
-                    # BulkDataEntry with name card_image
-                    scl, card = [(scl, scl(*field_data)) for scl in BulkDataEntry.__subclasses__()
-                                 if scl.__name__.upper() == card_image][0]
-                    try:
-                        cards_read[scl.__name__].append(card)
-                    except KeyError:
-                        cards_read[scl.__name__] = [card]
-
+                    bulk_data_entries = read_bulk_data(f)
         if not in_bulk:
             # BEGIN BULK is a required card for any valid Nastran job
             raise EOFError('No "BEGIN BULK" statement found.')
@@ -73,3 +49,30 @@ def read_card(line: str, f) -> list:
             else:
                 line = next(f)
     return tuple(card_data)
+
+
+def read_bulk_data(f) -> dict:
+    valid_cards = [scl.__name__.upper() for scl in BulkDataEntry.__subclasses__()]
+    cards_read = {}
+    for line in f:
+        # skip blank lines and comments
+        if not line.strip() or line.strip().startswith("$"):
+            pass
+        else:
+            card_image = line[:8].strip().upper()
+            # Warn when unsupported card encountered
+            if card_image not in valid_cards:
+                warnings.warn(f'Unsupported card image found: {card_image}')
+            else:
+                field_data = read_card(line, f)
+                # TODO see if theres a way to call a subclass directly (without having to import every subclass)
+                # There's no need for this to be a list comprehension, I just want to run the subclass of
+                # BulkDataEntry with name card_image
+                scl, card = [(scl, scl(*field_data)) for scl in BulkDataEntry.__subclasses__()
+                             if scl.__name__.upper() == card_image][0]
+                try:
+                    cards_read[scl.__name__].append(card)
+                except KeyError:
+                    # First card of this type. Create new entry in dictionary.
+                    cards_read[scl.__name__] = [card]
+    return cards_read
