ba2a9b909a2b6aa271aedd3b98d46942400a70f6
Josiah Lund <josiahlund@gmail.com>
2021-08-04 10:52:02 -0500

    Format grid card data

--- BULKDATA/grid/grid.py
+++ BULKDATA/grid/grid.py
@@ -3,11 +3,19 @@ from BULKDATA.BulkDataEntry import BulkDataEntry
 
 class Grid(BulkDataEntry):
     def __init__(self, ID, CP, X1, X2, X3, CD, PS, SEID):
-        self.ID = ID
-        self.CP = CP
+        self.ID = int(ID)
+        self.CP = int(CP) if CP != "" else 0
         self.X1 = X1
         self.X2 = X2
         self.X3 = X3
-        self.CD = CD
+        self.CD = int(CD) if CD != "" else 0
         self.PS = PS
-        self.SEID = SEID
+        self.SEID = int(SEID) if SEID != "" else 0
+        self.dtype = [("ID", "int"),
+                      ("CP", "int"),
+                      ("X1", "float"),
+                      ("X2", "float"),
+                      ("X3", "float"),
+                      ("CD", "int"),
+                      ("PS", "S8"),
+                      ("SEID", "int")]
