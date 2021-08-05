5824f5a44d3038ed0b64496f22f6ffba2cddd58b
Josiah Lund <josiahlund@gmail.com>
2021-08-04 22:42:30 -0500

    Sort data

--- SCRIPTS/write_hdf5.py
+++ SCRIPTS/write_hdf5.py
@@ -8,6 +8,6 @@ def write_hdf5(path: str, bulk_data_entries: dict):
         where = value[0].h5_path
         dtype = value[0].dtype
         data = [(tuple(card.__dict__.values())[:8]) for card in bulk_data_entries[key]]
-        cards = np.array(data, dtype=dtype)
+        cards = np.sort(np.array(data, dtype=dtype), order="ID")
         hdf5.create_dataset(where, data=cards, dtype=dtype)
     hdf5.close()
