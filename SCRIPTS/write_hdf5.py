6229b7ac46af5c0f8191bc80bb2513a20cc4e66c
Josiah Lund <josiahlund@gmail.com>
2021-08-05 10:23:15 -0500

    Add h5 attribute

--- SCRIPTS/write_hdf5.py
+++ SCRIPTS/write_hdf5.py
@@ -4,6 +4,8 @@ import numpy as np
 
 def write_hdf5(path: str, bulk_data_entries: dict):
     hdf5 = h5py.File(path, mode="w")
+    schema_value = 1
+    hdf5.attrs.create("SCHEMA", np.array([schema_value], dtype="int64"))
     for key, value in bulk_data_entries.items():
         where = value[0].h5_path
         dtype = value[0].dtype + [("DOMAIN_ID", int)]
