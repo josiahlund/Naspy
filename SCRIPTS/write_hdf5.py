ae11fcb50bd121e25893a12159c2e959c1ecabd1
Josiah Lund <josiahlund@gmail.com>
2021-08-04 23:29:56 -0500

    Add DOMAIN_ID field

--- SCRIPTS/write_hdf5.py
+++ SCRIPTS/write_hdf5.py
@@ -6,9 +6,9 @@ def write_hdf5(path: str, bulk_data_entries: dict):
     hdf5 = h5py.File(path, mode="w")
     for key, value in bulk_data_entries.items():
         where = value[0].h5_path
-        dtype = value[0].dtype
+        dtype = value[0].dtype + [("DOMAIN_ID", int)]
         count = value[0].entry_count
-        data = [(tuple(card.__dict__.values())[:count]) for card in bulk_data_entries[key]]
+        data = [(tuple(card.__dict__.values())[:count] + (1,)) for card in bulk_data_entries[key]]
         cards = np.sort(np.array(data, dtype=dtype), order="ID")
         hdf5.create_dataset(where, data=cards, dtype=dtype)
     hdf5.close()
