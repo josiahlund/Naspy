7efcc3172e246286d4f7330e7da1ffd8f18a3318
Josiah Lund <josiahlund@gmail.com>
2021-08-04 23:11:49 -0500

    Group grid coordinates

--- BULKDATA/grid/grid.py
+++ BULKDATA/grid/grid.py
@@ -5,18 +5,15 @@ class Grid(BulkDataEntry):
     def __init__(self, ID, CP, X1, X2, X3, CD, PS, SEID):
         self.ID = int(ID)
         self.CP = int(CP) if CP != "" else 0
-        self.X1 = X1
-        self.X2 = X2
-        self.X3 = X3
+        self.X = [X1, X2, X3]
         self.CD = int(CD) if CD != "" else 0
-        self.PS = PS
+        self.PS = int(PS) if PS != "" else 0
         self.SEID = int(SEID) if SEID != "" else 0
+        self.entry_count = 6
         self.h5_path = "/NASTRAN/INPUT/NODE/GRID"
-        self.dtype = [("ID", "int"),
-                      ("CP", "int"),
-                      ("X1", "float"),
-                      ("X2", "float"),
-                      ("X3", "float"),
-                      ("CD", "int"),
-                      ("PS", "S8"),
-                      ("SEID", "int")]
+        self.dtype = [("ID", int),
+                      ("CP", int),
+                      ("X", float, 3),
+                      ("CD", int),
+                      ("PS", int),
+                      ("SEID", int)]
--- SCRIPTS/write_hdf5.py
+++ SCRIPTS/write_hdf5.py
@@ -7,7 +7,8 @@ def write_hdf5(path: str, bulk_data_entries: dict):
     for key, value in bulk_data_entries.items():
         where = value[0].h5_path
         dtype = value[0].dtype
-        data = [(tuple(card.__dict__.values())[:8]) for card in bulk_data_entries[key]]
+        count = value[0].entry_count
+        data = [(tuple(card.__dict__.values())[:count]) for card in bulk_data_entries[key]]
         cards = np.sort(np.array(data, dtype=dtype), order="ID")
         hdf5.create_dataset(where, data=cards, dtype=dtype)
     hdf5.close()
