46c67088d78263a154feff86bf16853fd1ef0556
Josiah Lund <josiahlund@gmail.com>
2021-08-25 21:39:54 -0500

    Convert grid coords to floats

--- BULKDATA/grid/grid.py
+++ BULKDATA/grid/grid.py
@@ -5,7 +5,7 @@ class Grid(BulkDataEntry):
     def __init__(self, ID, CP, X1, X2, X3, CD, PS, SEID):
         self.ID = int(ID)
         self.CP = int(CP) if CP != "" else 0
-        self.X = [X1, X2, X3]
+        self.X = [float(X1), float(X2), float(X3)]
         self.CD = int(CD) if CD != "" else 0
         self.PS = int(PS) if PS != "" else 0
         self.SEID = int(SEID) if SEID != "" else 0
