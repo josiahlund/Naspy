1a71026aa425c7b635886e98d07a6aed56270dfa
Josiah Lund <josiah.lund@boeing.com>
2022-06-13 21:09:46 -0500

    Merge read_bdf and read_bulk_data functions

--- SCRIPTS/read_bdf.py
+++ SCRIPTS/read_bdf.py
@@ -5,6 +5,9 @@ from tkinter import filedialog
 
 
 def read_bdf(path: str) -> object:
+    valid_cards = registry.keys()
+    bulk_data = {card: {} for card in valid_cards}
+
     with open(path) as f:
         # If there is a "BEGIN BULK" statement anywhere within the file being read, assume the file also contains pre-
         # bulk entries (Nastran statments, executive control, file management statements, case control statements) which
@@ -22,44 +25,36 @@ def read_bdf(path: str) -> object:
                 if line.strip().upper() == "BEGIN BULK":
                     in_bulk = True
                 line = next(f)
-            bulk_data_entries = read_bulk_data(f)
-            break
-    return bulk_data_entries
-
-
-def read_bulk_data(f: object) -> dict:
-    valid_cards = registry.keys()
-    bulk_data = {card: {} for card in valid_cards}
-    for line in f:
-        # skip blank lines and comments
-        if not line.strip() or line.strip().startswith("$"):
-            pass
-        else:
-            if line.startswith("ENDDATA"):
-                break
-            card_image = line[:8].strip().upper()
-            # Warn when unsupported card encountered if not a continuation line
-            if card_image not in valid_cards:
-                if not card_image.startswith("*") and not card_image.startswith("+"):
-                    warnings.warn(f'Unsupported card image found: {card_image}')
+            # skip blank lines and comments
+            if not line.strip() or line.strip().startswith("$"):
+                pass
             else:
-                field_data = read_card(line, f)
-                card = registry[card_image](*field_data)
-                # Check if entry already exists to prevent overwriting data (LBYL)
-                if (ID := int(field_data[0])) in bulk_data[card_image]:
-                    # If this card already exists but with different fields, raise a warning
-                    if repr(bulk_data[card_image][ID]) != repr(card):
-                        # TODO: add decorator or event handler to log all errors/warnings for the entire BDF, so a
-                        #       maximum number of issues can be identified with each use.
-                        # TODO: add detail to warning message to inform what field(s) have differences
-                        warnings.warn(f'Duplicate {card_image} entry of ID {ID} with difference in one or more fields')
-                        print(bulk_data[card_image][ID])
-                        print(card)
-                    else:
-                        # data is identical to the data which has already been read, no need to read again.
-                        pass
+                if line.startswith("ENDDATA"):
+                    break
+                card_image = line[:8].strip().upper()
+                # Warn when unsupported card encountered if not a continuation line
+                if card_image not in valid_cards:
+                    if not card_image.startswith("*") and not card_image.startswith("+"):
+                        warnings.warn(f'Unsupported card image found: {card_image}')
                 else:
-                    bulk_data[card_image][ID] = card
+                    field_data = read_card(line, f)
+                    card = registry[card_image](*field_data)
+                    # Check if entry already exists to prevent overwriting data (LBYL)
+                    if (ID := int(field_data[0])) in bulk_data[card_image]:
+                        # If this card already exists but with different fields, raise a warning
+                        if repr(bulk_data[card_image][ID]) != repr(card):
+                            # TODO: add decorator or event handler to log all errors/warnings for the entire BDF, so a
+                            #       maximum number of issues can be identified with each use.
+                            # TODO: add detail to warning message to inform what field(s) have differences
+                            warnings.warn(
+                                f'Duplicate {card_image} entry of ID {ID} with difference in one or more fields')
+                            print(bulk_data[card_image][ID])
+                            print(card)
+                        else:
+                            # data is identical to the data which has already been read, no need to read again.
+                            pass
+                    else:
+                        bulk_data[card_image][ID] = card
     return bulk_data
 
 
